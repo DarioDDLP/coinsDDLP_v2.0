5ad960cdef4ef03705dbd2ad540d3fd30b4225ae
Darío de la Poza <dariodelapoza@Mac-mini-M2-Pro.local>
2026-04-11 15:22:19 +0200

    feat(shared): add login button

--- coins-ddlp-front/src/app/shared/components/sidebar/sidebar.component.ts
+++ coins-ddlp-front/src/app/shared/components/sidebar/sidebar.component.ts
@@ -3,11 +3,12 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 import { SIDEBAR_ITEMS, SidebarItem } from './sidebar.config';
 import { LITERALS } from '../../constants/literals';
 import { CountryFlagComponent } from '../country-flag/country-flag.component';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, CountryFlagComponent],
+  imports: [RouterLink, RouterLinkActive, CountryFlagComponent, ButtonComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
