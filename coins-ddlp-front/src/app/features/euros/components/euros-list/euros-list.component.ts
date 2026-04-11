d0375663a3121098940e1096534d32a89cb21b52
Darío de la Poza <dariodelapoza@Mac-mini-M2-Pro.local>
2026-04-11 15:31:13 +0200

    feat(euros): move background to EurosListComponent)

--- coins-ddlp-front/src/app/features/euros/components/euros-list/euros-list.component.ts
+++ coins-ddlp-front/src/app/features/euros/components/euros-list/euros-list.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-euros-list',
   standalone: true,
-  template: `<p>euros-list — TODO</p>`,
+  templateUrl: './euros-list.component.html',
+  styleUrl: './euros-list.component.scss',
 })
 export class EurosListComponent {}
